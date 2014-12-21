af32c2a668f2da90711f44cde51d0bcee3a7edbf
kathegaara <aditya.arca@gmail.com>
2014-12-21 14:04:55 +0530

    Adding the two funtions

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that creates a list of functions on a special matirx to
+## set the value of the matrixx
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
@@ -12,4 +28,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inv <- x$getinv()
+            if(!is.null(inv)) {
+                    message("getting cached data")
+                    return(inv)
+            }
+            data <- x$get()
+            inv <- solve(data)
+            x$setinv(inv)
+            inv
 }
+
+
+https://github.com/kathegaara/ProgrammingAssignment2.git
